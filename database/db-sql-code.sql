af8cb4b5b1eb221f627c55bc94155ff146f17d3a
cvargas306 <cavbcr@gmail.com>
2025-09-13 17:55:55 -0600

    Update database documents, add assignment2.sql file

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -245,10 +245,15 @@ VALUES   (
   );
  
  
-UPDATE inventory SET inv_image=replace(inv_image,'/images','/images/vehicles'), inv_thumbnail=replace(inv_thumbnail, '/images', '/images/vehicles');
- 
 UPDATE public.inventory
-  SET inv_description = replace(inv_description,
-    'small interiors',
-    'a huge interior')
-    WHERE inv_id = 10;
+SET inv_description = REPLACE(
+        inv_description,
+        'small interiors',
+        'a huge interior'
+    )
+WHERE inv_make = 'GM'
+    AND inv_model = 'Hummer';
+
+UPDATE public.inventory 
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
