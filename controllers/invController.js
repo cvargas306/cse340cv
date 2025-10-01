33fc5fd99cc6ebb8c36f28b851a362bc0ae0b3a1
cvargas306 <cavbcr@gmail.com>
2025-10-01 00:13:36 -0600

    Add new category and new vehicle logic

--- controllers/invController.js
+++ controllers/invController.js
@@ -42,4 +42,129 @@ invCont.buildByInventoryId = async function (req, res, next) {
   })
 }
 
+invCont.buildManagement = async function (req, res, next) {
+  let nav = await utilities.getNav()
+  let classificationSelect = await utilities.buildClassificationList()
+  res.render("./inventory/management", {
+    title: "Vehicle Management",
+    nav,
+    errors: null,
+    classificationSelect
+  })
+}
+
+// Build add classification view
+invCont.buildAddClassification = async function (req, res, next) {
+  let nav = await utilities.getNav()
+  res.render("./inventory/add-classification", {
+    title: "Add New Classification",
+    nav,
+    errors: null
+  })
+}
+
+// Build add inventory view
+invCont.buildAddInventory = async function (req, res, next) {
+  let nav = await utilities.getNav()
+  let classificationSelect = await utilities.buildClassificationList()
+  res.render("./inventory/add-inventory", {
+    title: "Add New Vehicle",
+    nav,
+    errors: null,
+    classifications: classificationSelect
+  })
+}
+
+/* ***************************
+ *  Process add classification
+ * ************************** */
+invCont.processAddClassification = async function (req, res, next) {
+  const { classification_name } = req.body
+  
+  const addResult = await invModel.addClassification(classification_name)
+  
+  if (addResult) {
+    req.flash("notice", `The ${classification_name} classification was successfully added.`)
+    let nav = await utilities.getNav() // Rebuild nav to show new classification
+    let classificationSelect = await utilities.buildClassificationList()
+    res.status(201).render("./inventory/management", {
+      title: "Vehicle Management",
+      nav,
+      errors: null,
+      classificationSelect
+    })
+  } else {
+    req.flash("notice", "Sorry, adding the classification failed.")
+    let nav = await utilities.getNav()
+    res.status(501).render("./inventory/add-classification", {
+      title: "Add New Classification",
+      nav,
+      errors: null,
+      classification_name
+    })
+  }
+}
+
+/* ***************************
+ *  Process add inventory
+ * ************************** */
+invCont.processAddInventory = async function (req, res, next) {
+  const { 
+    inv_make, 
+    inv_model, 
+    inv_year, 
+    inv_description, 
+    inv_image, 
+    inv_thumbnail, 
+    inv_price, 
+    inv_miles, 
+    inv_color, 
+    classification_id 
+  } = req.body
+  
+  const addResult = await invModel.addInventory(
+    inv_make, 
+    inv_model, 
+    inv_year, 
+    inv_description, 
+    inv_image, 
+    inv_thumbnail, 
+    inv_price, 
+    inv_miles, 
+    inv_color, 
+    classification_id
+  )
+  
+  if (addResult) {
+    req.flash("notice", `The ${inv_make} ${inv_model} was successfully added.`)
+    let nav = await utilities.getNav()
+    let classificationSelect = await utilities.buildClassificationList()
+    res.status(201).render("./inventory/management", {
+      title: "Vehicle Management",
+      nav,
+      errors: null,
+      classificationSelect
+    })
+  } else {
+    req.flash("notice", "Sorry, adding the vehicle failed.")
+    let nav = await utilities.getNav()
+    let classificationSelect = await utilities.buildClassificationList()
+    res.status(501).render("./inventory/add-inventory", {
+      title: "Add New Vehicle",
+      nav,
+      errors: null,
+      classifications: classificationSelect,
+      inv_make, 
+      inv_model, 
+      inv_year, 
+      inv_description, 
+      inv_image, 
+      inv_thumbnail, 
+      inv_price, 
+      inv_miles, 
+      inv_color
+    })
+  }
+}
+
 module.exports = invCont
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -6,8 +6,20 @@ const utilities = require("../utilities/")
 
 // Route to build inventory by classification view
 router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
+router.get("/add-classification", utilities.handleErrors(invController.buildAddClassification));
 
 // Route to build vehicle detail view
 router.get("/detail/:invId", utilities.handleErrors(invController.buildByInventoryId));
+
+// Route to management view
+router.get("/", utilities.handleErrors(invController.buildManagement));
+
+router.get("/add-inventory", utilities.handleErrors(invController.buildAddInventory));
+
+// POST route to process new classification
+router.post("/add-classification", utilities.handleErrors(invController.processAddClassification));
+
+// POST route to process new inventory
+router.post("/add-inventory", utilities.handleErrors(invController.processAddInventory));
 
 module.exports = router;
