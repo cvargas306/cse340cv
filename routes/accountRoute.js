c39ca1f0688092702370ee759cf95ed204d16eca
cvargas306 <cavbcr@gmail.com>
2025-10-01 21:46:27 -0600

    Add route middleware

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -5,7 +5,7 @@ const router = new express.Router()
 const accountController = require("../controllers/accountController");
 const utilities = require("../utilities/")
 
-router.get("/", utilities.handleErrors(accountController.buildAccountManagementView));
+router.get("/", utilities.checkLogin, utilities.handleErrors(accountController.buildAccountManagementView));
 
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 
